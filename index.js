480c68d2c9b69b9305b1926ca2910213b50a11aa
root <root@nhti3-64.nhti.edu>
2017-02-27 21:20:22 -0500

    work on client

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var output;
-var employees;
-var jobs;
+var emps = {};
+var jobs = {};
+var pubs = {};
+var 
 
 function add_employee(emp)
 {
@@ -8,30 +10,100 @@ function add_employee(emp)
     var delim = '</td><td>';
     var delim_center = '</td><td class="text-center">';
     var end = '</td></tr>';
-    var edit = '<a href="#" class="edit-button" id="' + id + '"><span class="glyphicon glyphicon-pencil"></span></a>';
-    output.append(begin + emp.fname + delim + emp.lname + delim + jobs[emp.job_id] + delim + emp.level + delim + emp.employer + delim_center + edit + end);
+    var edit = '<a href="#" class="edit-button" id="' + emp.emp_id + '"><span class="glyphicon glyphicon-pencil"></span></a>';
+    emps[emp.emp_id] = emp;
+    output.append(begin + emp.fname + delim + emp.lname + delim + jobs[emp.job_id].job_desc + delim + emp.job_lvl + delim + pubs[emp.pub_id] + delim_center + edit + end);
 }
 
 function refresh()
 {
     output = $( '#authors' ).first();
 
-    jobs['001'] = 'Guy';
-    jobs['002'] = 'Ex-President';
+    $.getJSON('jobs.php', processJobs);
+}
 
-    add_employee({'John', 'Cena', 'Guy', 9001, 'WWE', 001);
-    add_employee('Barack', 'Obama', 'Ex-President', 12, 'AMERICA', 002);
+function processJobs(_jobs) {
+    for (var i = 0; i < _jobs.length; i++)
+    {
+        var job = _jobs[i];
+        jobs[job.job_id] = job;
+    }
+
+    $.getJSON('pubs.php', processPubs);
+    
+}
+
+function processPubs(_pubs) {
+    for (var i = 0; i < _pubs.length; i++)
+    {
+        var pub = _pubs[i];
+        pubs[pub.pub_id] = pub.pub_name;
+    }
+
+    $.getJSON('view.php', processView);
+}
+
+function processView(_view)
+{
+    for (var i = 0; i < _view.length; i++)
+    {
+        var emp = _view[i];
+        add_employee(emp);
+    }
+
+    $.each(jobs, function(key, value) {
+        $( '#job_id' )
+            .append($( '<option></option>' )
+                        .attr('value', key)
+                        .text(value.job_desc));
+    });
+
+    $.each(pubs, function(key, value) {
+        $( '#pub_id' )
+            .append($( '<option></option>' )
+                        .attr('value', key)
+                        .text(value));
+    });
+
+    $( '.edit-button' ).click(
+        function() {
+            $( '#edit-popup' ).removeClass('popup-hidden');
+            var id = $( this ).attr('id');
+            console.log(id);
+            var emp = emps[id];
+            console.log(emp);
+            $( '#fname' ).val(emp.fname);
+            $( '#lname' ).val(emp.lname);
+            $( '#job_id' ).val(emp.job_id);
+            $( '#job_lvl' ).val(emp.job_lvl);
+            $( '#pub_id' ).val(emp.pub_id);
+
+            $( '#job_lvl' ).attr('min', jobs[$( '#job_id' ).val()].min_lvl);
+            $( '#job_lvl' ).attr('max', jobs[$( '#job_id' ).val()].max_lvl);
+        }
+    );
+
+    $( '#job_id' ).change(
+        function() {
+            $( '#job_lvl' ).attr('min', jobs[$( '#job_id' ).val()].min_lvl);
+            $( '#job_lvl' ).attr('max', jobs[$( '#job_id' ).val()].max_lvl);
+            if ($( '#job_lvl' ).val() < $( '#job_lvl' ).attr('min'))
+            {
+                $( '#job_lvl' ).val($( '#job_lvl').attr('min'));
+            }
+            if ($( '#job_lvl' ).val() > $( '#job_lvl' ).attr('max'))
+            {
+                $( '#job_lvl' ).val($( '#job_lvl').attr('max'));
+            }
+        }
+    );
 }
 
 $( document ).ready(
     function() {
-
-        $( '.edit-button' ).click(
-            function() {
-                $( '#edit-popup' ).removeClass('popup-hidden');
-            }
-        );
-    });
+        refresh();
+    }
+);
 
 $( '.close-js' ).click(
     function() {
